270aec539a6dde009f3e397d2e1de7ccd1fcf92a
AnanyaPrabhav <68499485+AnanyaPrabhav@users.noreply.github.com>
2020-12-13 15:52:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+var ball, db, position;
+
+function setup(){
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+
+    db = firebase.database();
+    db.ref("ball/position").on("value", function(data){
+        position = data.val();
+        ball.x = position.x;
+        ball.y = position.y;
+    },
+    function(){
+        console.log("error");
+    }
+    )
+}
+
+function draw(){
+    background("white");
+    if(position !== undefined){
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+}
+    drawSprites();
+}
+
+function changePosition(x,y){
+    db.ref("ball/position").set({
+        x: position.x+x,
+        y: position.y+y
+    })
+}
